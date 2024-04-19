d94d1a4a5ce3b973f37f86f284b1d408c64d083c
chogerlate <siwaratlaopromger4444@gmail.com>
2024-04-19 23:05:06 +0700

    signin routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import "./App.css";
+import Signin from "./pages/signIn";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin/>} />
         </Routes>
       </div>
     </>
