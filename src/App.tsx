5b4b419ea131cc0e850fa30b30b0e5d96f17861a
Encall <apichataimimpat@gmail.com>
2024-04-21 15:06:49 +0700

    Add react-icons package to dependencies in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/AboutUs";
-import "./App.css";
-import SignupPage from "./pages/Signup";
-import LoginPage from "./pages/login";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/AboutUs';
+import './App.css';
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/login';
+import Navbar from './components/Navbar';
+import Footer from './components/footer';
+import Payment from './pages/Payment';
 
 function App() {
-  return (
-    <>
-      <div className="h-screen">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/AboutUs" element={<About />} />
-          <Route path="/signup" element={<SignupPage/>} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-        <Footer/>
-      </div>     
-    </>
-  );
+    return (
+        <>
+            <div className="h-screen">
+                <div className="">
+                    <Navbar />
+                </div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/AboutUs" element={<About />} />
+                    <Route path="/signup" element={<SignupPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/payment" element={<Payment />} />
+                </Routes>
+                <div className="relative">
+                    <Footer />
+                </div>
+            </div>
+        </>
+    );
 }
 export default App;
