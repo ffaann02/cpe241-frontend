80755128f15185bc3dddd5fb157dad75935b0062
Faan <rudfanno1@gmail.com>
2024-04-21 18:16:11 +0700

    add: vercel json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/AboutUs";
 import "./App.css";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
 import Navbar from "./components/layoutBar/Navbar";
 import Footer from "./components/layoutBar/Footer";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
         <div className="w-full h-full min-h-screen pt-[4.25rem] flex" id="app_container">
           <div className="flex-1 bg-slate-50">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
             <Route path="/signup" element={<SignupPage/>} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>
