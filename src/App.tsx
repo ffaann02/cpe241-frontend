c9741d805a805ef50b7cd17a915198ceffea64b6
Faan <rudfanno1@gmail.com>
2024-04-21 18:34:45 +0700

    fix: git config core ignore case

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
 import Navbar from "./components/layoutBar/Navbar";
 import Footer from "./components/layoutBar/Footer";
-import Home from "./pages/Home";
-import About from "./pages/AboutUs";
 
 function App() {
   return (
