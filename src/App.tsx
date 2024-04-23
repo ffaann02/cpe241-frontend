f217a93c1b5f46c541ae79b0548f47fc14159e29
Rudfaan Maimahad <84792604+ffaann02@users.noreply.github.com>
2024-04-23 15:27:33 +0700

    Revert "Feature/payment"

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import About from './pages/AboutUs';
-import SignupPage from './pages/Signup';
-import LoginPage from './pages/Login';
-import Navbar from './components/layoutBar/Navbar';
-import Footer from './components/layoutBar/Footer';
-import Home from './pages/Home';
-import Booking from './pages/Booking';
-import Payment from './pages/Payment';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import "./App.css";
 
 function App() {
-    return (
-        <>
-            <div className="w-full">
-                <Navbar />
-                <div className="w-full h-full min-h-screen pt-[4.25rem] flex" id="app_container">
-                    <div className="flex-1 bg-slate-50">
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/about" element={<About />} />
-                            <Route path="/signup" element={<SignupPage />} />
-                            <Route path="/login" element={<LoginPage />} />
-                            <Route path="/booking" element={<Booking />} />
-                            <Route path="/payment" element={<Payment />} />
-                        </Routes>
-                    </div>
-                </div>
-                <Footer />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
+
 export default App;
