3d8c98c36ed63ca0054620d8f2cccbac42311683
Faan <rudfanno1@gmail.com>
2024-04-24 00:36:19 +0700

    add: chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <App />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
     </BrowserRouter>
 );
