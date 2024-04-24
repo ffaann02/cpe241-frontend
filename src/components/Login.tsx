5fce536466608dca70b579c217f3bb029d730324
Encall <apichataimimpat@gmail.com>
2024-04-24 14:33:16 +0700

    Refactor Login component to include useEffect hook for API call and authentication token handling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { loginFields } from '../constants/formfield';
 import FormAction from './FormAction';
 import FormExtra from './FormExtra';
@@ -35,29 +35,29 @@ const Login: React.FC = () => {
         authenticateUser();
     };
 
-    const setAuthTokens = (data: { token: string }) => {
-        if (data.token) {
-            axios.defaults.headers.common['Authorization'] =
-                `Bearer ${data.token}`;
-        } else {
-            delete axios.defaults.headers.common['Authorization'];
-        }
-    };
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/airports').then((res) => {
+            console.log(res.data);
+        });
+    }, []);
 
     // Handle Login API Integration here
     async function authenticateUser() {
         try {
-            const response = await axios.post('http://localhost:3000/api/login', {
-                email: loginState.email,
-                password: loginState.password,
-            });
-            localStorage.setItem('token', response.data.token);
-            setAuthTokens(response.data);
+            const response = await axios.post(
+                'http://localhost:3000/api/login',
+                {
+                    email: loginState.email,
+                    password: loginState.password,
+                },
+                { withCredentials: true },
+            );
             console.log(response);
         } catch (error) {
             console.log(error);
         }
     }
+
     return (
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <div className="-space-y-px">
