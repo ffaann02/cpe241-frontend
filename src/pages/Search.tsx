a7d753d490eaeb815a8d10302ee391949230e078
Faan <rudfanno1@gmail.com>
2024-04-26 01:56:51 +0700

    add: flight search header with date carousel

---
+++ src/pages/Search.tsx
@@ -0,0 +1,10 @@
+import SearchHeader from "../components/searchPage/SearchHeader"
+
+const Search = () => {
+  return (
+    <div className="w-full h-full">
+        <SearchHeader/>
+    </div>
+  )
+}
+export default Search
