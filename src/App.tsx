22dd4ba45703bde828fbe1cc8077c29ac1e63274
chogerlate <siwaratlaopromger4444@gmail.com>
2024-04-29 00:55:59 +0700

    firstlook dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import About from './pages/AboutUs';
 import SignupPage from './pages/Signup';
@@ -9,12 +9,15 @@ import Booking from './pages/Booking';
 import Payment from './pages/Payment';
 import Footer from './components/layoutBar/Footer';
 import Search from './pages/Search';
+import RtlLayout from "./dashboard/layouts/rtl";
+import AdminLayout from "./dashboard/layouts/admin";
+import AuthLayout from "./dashboard/layouts/auth"; 
 
 function App() {
     return (
         <>
             <div className="w-full">
-                <Navbar isLoggedIn={false}/>
+                <Navbar isLoggedIn={false} />
                 <div className="w-full h-full min-h-screen pt-[3.8rem] flex" id="app_container">
                     <div className="flex-1 bg-slate-50">
                         <Routes>
@@ -24,11 +27,15 @@ function App() {
                             <Route path="/login" element={<LoginPage />} />
                             <Route path="/booking" element={<Booking />} />
                             <Route path="/payment" element={<Payment />} />
-                            <Route path="search" element={<Search/>}/>
+                            <Route path="search" element={<Search />} />
+                            <Route path="auth/*" element={<AuthLayout />} />
+                            <Route path="admin/*" element={<AdminLayout />} />
+                            <Route path="rtl/*" element={<RtlLayout />} />
+                            <Route path="/dashboard" element={<Navigate to="/admin" replace />} />
                         </Routes>
                     </div>
                 </div>
-                <Footer/>
+                <Footer />
             </div>
         </>
     );
