a16bfe664597c36242b5b83208a003675b63d8b5
Faan <rudfanno1@gmail.com>
2024-05-14 22:15:12 +0700

    hotfix: scroll employee board

--- src/dashboard/views/admin/employeeTable/components/employeeBoard.tsx
+++ src/dashboard/views/admin/employeeTable/components/employeeBoard.tsx
@@ -5,7 +5,7 @@ import { Flex } from '@chakra-ui/react'
 
 export default function employeeBoard() {
     return (
-        <div>
+        <div className="overflow-x-scroll">
             <Flex direction={"column"} mt={'3'} gap={2} w={"full"} border={'2px'} borderColor='gray.200' p={4} borderRadius={'5'} bg='white'>
                 <Search type='employee'/>
                 <EmployeeTable />
